c10db2570156bbe8a71916d6682fa16870146e93
Ethan Russell <ethan@ethan-russell.com>
2025-01-25 20:00:46 -0700

    Separate projects into different catagories

--- ethanrussellcontent.js
+++ ethanrussellcontent.js
@@ -1,8 +1,25 @@
 var coll = document.getElementsByClassName("collapsible");
+var autoOpen = document.getElementsByClassName("collapsible_auto");
 var i;
 
 for (i = 0; i < coll.length; i++) {
 	coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
+}
+
+for (i = 0; i < autoOpen.length; i++) {
+	var content = autoOpen[i].nextElementSibling;
+	content.style.display = "block";
+	autoOpen[i].classList.add("active");
+	
+	autoOpen[i].addEventListener("click", function() {
 		this.classList.toggle("active");
 		var content = this.nextElementSibling;
 		if (content.style.display === "block") {
